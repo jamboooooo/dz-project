7b4d27c51e4bb5cb63b016db4b5b946f285a7c67
jambo <hisllam35@gmail.com>
2023-10-22 12:50:42 +0300

    add e2e test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
-    specPattern: ["cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", "src/**/*.cy.{js,jsx,ts,tsx}"]
+    baseUrl: "http://localhost:5173",
+    specPattern: [
+      "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+      "src/**/*.cy.{js,jsx,ts,tsx}",
+    ],
   },
 
   component: {
